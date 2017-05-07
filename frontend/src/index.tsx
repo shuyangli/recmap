68efc7c70d454f78d0b992fe9449dd5f6d1038ad
Shuyang Li <shuyang.li.95@gmail.com>
2017-05-06 21:25:30 -0700

    Wire up css

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -5,6 +5,8 @@ import { Provider } from 'react-redux';
 import { App } from './components/App';
 import { store } from './reducers/store';
 
+import './index.less';
+
 ReactDOM.render(
   <Provider store={store}>
     <App />
