7ea3dc4174a6af18dd72bed63e3a180654796b4c
Shuyang Li <shuyang.li.95@gmail.com>
2017-08-13 15:41:52 -0700

    Add webpack dev server setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,24 @@
 "use strict";
 
+const path = require("path");
+
 module.exports = {
   entry: "./src/index.tsx",
   output: {
     filename: "app.js",
-    path: __dirname + "/build"
+    path: path.resolve(__dirname, "build"),
+    publicPath: "/build"
   },
   resolve: {
     extensions: [".ts", ".tsx", ".js"]
   },
   module: {
     rules: [
-      { test: /\.tsx?$/, exclude: /node_modules/, loader: "ts-loader" },
-      { test: /\.less$/, exclude: /node_modules/, loader: ["style-loader", "css-loader", "less-loader"] },
-      { test: /\.css$/, exclude: /node_modules/, loader: ["style-loader", "css-loader"] },
-      { test: /\.(eot|ttf|woff|woff2)$/, loader: require.resolve("file-loader") + "?name=assets/fonts/[name].[ext]" }
+      { test: /\.tsx?$/, exclude: /node_modules/, use: "ts-loader" },
+      { test: /\.less$/, exclude: /node_modules/, use: ["style-loader", "css-loader", "less-loader"] },
+      { test: /\.css$/, exclude: /node_modules/, use: ["style-loader", "css-loader"] },
+      { test: /\.(eot|ttf|woff|woff2)$/, use: "file-loader" }
     ]
   },
-  externals: {
-    "react": "React",
-    "react-dom": "ReactDOM"
-  }
+  devtool: "inline-source-map"
 }
